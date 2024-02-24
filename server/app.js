a3909e778e8db2587e7ccbea4d0ed54f016be65f
adridalo <adrianodalonzo@gmail.com>
2024-02-24 02:25:23 -0500

    Change TTL to 1 day

--- server/app.js
+++ server/app.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const { OAuth2Client } = require("google-auth-library");
-const User = require('./db/models')
 const session = require("express-session");
 const generateSecret = require("./util/util");
 const MongoStore = require("connect-mongo");
-const {checkAuthentication, login} = require("./controllers/auth");
 const authRouter = require('./routes/auth')
-
-if(!process.env.SESSION_SECRET) {
-    process.env.SESSION_SECRET = generateSecret();
-}
 
 const app = express();
 app.use(express.json())
@@ -20,7 +13,7 @@ app.use(session({
     store: MongoStore.create({
         mongoUrl: process.env.DB_URL,
         dbName: 'fluttr',
-        ttl: 60 * 60 * 1000
+        ttl: 86400000
     })
 }))
 
