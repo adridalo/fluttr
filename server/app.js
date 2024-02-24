7b9d8f48e08275ed99409066151c7564aa22359d
adridalo <adrianodalonzo@gmail.com>
2024-02-23 23:35:31 -0500

    Setup login for google

--- server/app.js
+++ server/app.js
@@ -1,17 +1,61 @@
 const express = require('express');
-require('google-auth-library')
+const {OAuth2Client} = require("google-auth-library");
+const User = require('./db/models')
+const session = require("express-session");
+const crypto = require('crypto');
+
+const generateSecret = () => {
+    return crypto.randomBytes(32).toString('hex');
+}
+
+if(!process.env.SESSION_SECRET) {
+    process.env.SESSION_SECRET = generateSecret();
+}
 
 const app = express();
 app.use(express.json())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
 
 app.get('/api/', (req, res) => {
     res.json('Hello World!');
 })
 
-app.post('/api/login', (req, res) => {
-    const results = req.body
+app.post('/api/login', async (req, res) => {
+    const { credential, clientId, _ } = req.body
 
-    console.log(results)
+    async function verify(clientId, jwtToken) {
+        const client = new OAuth2Client(clientId)
+        const ticket = await client.verifyIdToken({
+            idToken: jwtToken,
+            audience: clientId
+        })
+
+        return ticket.getPayload()
+    }
+
+    const payload = await verify(clientId, credential)
+    if(payload) {
+        const user = await User.updateOne({
+            email: payload.email
+        }, {
+            $set: {
+                email: payload.email,
+                name: payload.name,
+                pfp: payload.picture
+            }
+        }, {
+            upsert: true
+        })
+
+        req.session.userId = user.email
+
+        user.save()
+    }
+
 })
 
 module.exports = app;
--- server/db/models.js
+++ server/db/models.js
@@ -1,36 +1,19 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: {
+    email: {
         type: String,
         required: true,
         unique: true
     },
-    password: {
+    name: {
+        type: String,
+        required: true
+    },
+    pfp: {
         type: String,
         required: true
     }
 });
-
-userSchema.pre('save', async function (next) {
-    try {
-        // Only hash the password if it has been modified or is new
-        if (!this.isModified('password')) next()
-
-        // Generate a salt
-        const salt = await bcrypt.genSalt(10);
-
-        // Hash the password with the generated salt
-        const hashedPassword = await bcrypt.hash(this.password, salt);
-
-        // Replace the plain password with the hashed one
-        this.password = hashedPassword;
-        next();
-    } catch (error) {
-        next(error);
-    }
-});
-
 
 module.exports = mongoose.model('User', userSchema)
